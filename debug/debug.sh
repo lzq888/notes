93b8239283f20c5ffffaba8dc8f84a632ee241b2
Mike Thompson <mpthompson@gmail.com>
2014-10-18 01:37:54 -0700

    Switched to new debugger.

--- debug/debug.sh
+++ debug/debug.sh
@@ -10,10 +10,10 @@ XTERM_PID=$!
 sleep 2
 
 # execute some initialisation commands via gdb
-arm-none-eabi-gdb --batch --command=init.gdb vmlinux
+arm-none-linux-gnueabi-gdb --batch --command=init.gdb vmlinux
 
 # start the gdb gui
-nemiver --remote=localhost:2331 --gdb-binary="$(which arm-none-eabi-gdb)" vmlinux
+nemiver --remote=localhost:2331 --gdb-binary="$(which arm-none-linux-gnueabi-gdb)" vmlinux
 
 # close xterm when the user has exited nemiver
 kill $XTERM_PID
